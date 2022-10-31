4ca8b67b36fbe35c17533f1e4201c2b6c4f27334
Alowree <alowree@hotmail.com>
2022-10-31 15:41:29 +0800

    Site updated: 2022-10-31 15:41:29

---
+++ js/markmap.js
@@ -0,0 +1,55 @@
+
+  const initMarkmap = async () => {
+    const debounce = (callback, wait) => {
+      let timeout;
+      return function (...args) {
+          const context = this;
+          clearTimeout(timeout);
+          timeout = setTimeout(() => callback.apply(context, args), wait);
+      };
+    }
+    const autoFit = (markmaps) => {
+      window.addEventListener('resize', () => debounce(Promise.all(markmaps.map(markmap => markmap.fit() )), 100))
+    }
+    const createMarkmap = () => {
+      const markmaps = []
+      document.querySelectorAll('.markmap-container>svg').forEach(el => {
+        markmaps.push(markmap.Markmap.create(el, {autoFit: true}, JSON.parse(el.getAttribute('data'))))
+      })
+      autoFit(markmaps)
+    }
+    if (window.markmap && Object.keys(window.markmap).length != 0) { createMarkmap(); return }
+    const CDN = {
+      "js": {
+        "d3": 'https://fastly.jsdelivr.net/npm/d3@6',
+        "markmap_view": 'https://fastly.jsdelivr.net/npm/markmap-view@0.2.7',
+      },
+      "css": [
+        
+        
+      ],
+    }
+    const loadElement = (elname, attr) => {
+      const el = document.createElement(elname)
+      for (let [name, value] of Object.entries(attr)) {
+        el[name] = value
+      }
+      document.body.appendChild(el);
+      return new Promise((res, rej) => {
+        el.onload = res
+        el.onerror = rej
+      })
+    }
+    const loadScript = (url) => loadElement('script', { 'src': url })
+    const loadCSS = (url) => loadElement('link', { 'href': url, 'rel': "stylesheet" })
+    const loadStyle = (style) => document.head.insertAdjacentHTML("beforeend", "<style>.markmap-container{display:flex;justify-content:center;margin:0 auto;width:90%;height:500px}.markmap-container svg{width:100%;height:100%}@media(max-width:768px){.markmap-container{height:400px}}</style>")
+    loadStyle()
+    await loadScript(CDN.js.d3)
+    await loadScript(CDN.js.markmap_view)
+    await Promise.all(CDN.css.map(loadCSS))
+
+    createMarkmap()
+}
+if(document.querySelector('.markmap-container')) {
+  initMarkmap()
+}
